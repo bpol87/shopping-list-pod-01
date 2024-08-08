506fc44ab468eaef9d75c32d7db22cac637d1417
Emma Molden <emma.molden007@gmail.com>
2024-08-08 14:02:28 -0500

    POST Route working

--- server/routes/shoppingList.router.js
+++ server/routes/shoppingList.router.js
@@ -1,14 +1,14 @@
 // A ROUTER!!!!
 
 const express = require('express')
-const pool = require('../modules/pool')
+const pool = require('../modules/pool.js')
 
 const router = express.Router()
 
-// GET /api/students
+// GET /api/shoppingList
 router.get('/', (req, res) => {
     const sqlText = `
-      SELECT * FROM "shoppingList"
+      SELECT * FROM shoppingList
         ORDER BY "id";
     `
   
@@ -23,16 +23,35 @@ router.get('/', (req, res) => {
       })
   })
 
-  module.exports = router;
+
 
 // POST route
+router.post('/', (req, res) => {
+    const sqlText = `
+    INSERT INTO shoppingList
+        ("name", "quantity", "unit")
+        VALUES
+        ($1, $2, $3)
+    `;
+    const sqlValues = [req.body.name, req.body.quantity, req.body.unit]
+
+    pool.query(sqlText, sqlValues)
+    .then((dbRes) => {
+        res.sendStatus(201);
+    })
+    .catch((dbErr) => {
+        console.log('Database error in POST /api/shipping-list', dbErr)
+        res.sendStatus(500);
+    })
+
+})
 
 // DELETE route
 router.delete('/:id', (req, res) => {
     console.log('DELETE /api/shopping-list/:id received a request!')
 
     const sqlText = `
-          DELETE FROM "shoppingList"
+          DELETE FROM shoppingList
               WHERE id = $1;
       `;
     const sqlValues = [req.params.id];
@@ -52,7 +71,7 @@ router.put('/:id', (req, res) => {
     console.log('PUT /api/shopping-list/:id received a request!');
 
     const sqlText = `
-      UPDATE "shoppinglist"
+      UPDATE shoppinglist
         SET "name" = $1, "quantity" = $2, "unit" = $3
         WHERE "id" = $4;
     `
@@ -68,3 +87,5 @@ router.put('/:id', (req, res) => {
             res.sendStatus(500);
         })
 })
+
+module.exports = router;
